4caa6092f01507f5a7081c22a32b45cc67cf2396
juhoPark <wngh7247@gmail.com>
2020-05-22 21:31:20 +0900

    fixed untracked files

---
+++ src/main/java/com/neighborhoodclub/api/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.neighborhoodclub.api.security;
+
+import com.neighborhoodclub.api.domain.users.User;
+import com.neighborhoodclub.api.domain.users.UserRepository;
+import com.neighborhoodclub.api.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserPrincipal loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
