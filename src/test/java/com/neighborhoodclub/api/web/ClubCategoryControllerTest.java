cc8e775cd8a09318440f9c3574301d44efa98c92
juhoPark <wngh7247@gmail.com>
2020-06-03 15:24:17 +0900

    travisCI 등록

---
+++ src/test/java/com/neighborhoodclub/api/web/ClubCategoryControllerTest.java
@@ -0,0 +1,36 @@
+package com.neighborhoodclub.api.web;
+
+import com.neighborhoodclub.api.domain.clubCategories.ClubCategoryRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClubCategoryControllerTest {
+
+    @Autowired
+    ClubCategoryRepository clubCategoryRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
+}
